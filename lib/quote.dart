6562ebc0855b835f2e356d43c62ae56ed1693ce9
Jaclyn Chen <jaclyn.chen5@gmail.com>
2020-12-01 19:06:34 -0500

    named parameters shortened

--- lib/quote.dart
+++ lib/quote.dart
@@ -3,11 +3,6 @@ class Quote{
   String text;
   String author;
 
-  Quote({String text, String author}){ //constructor same name as class, named parameters
-    this.text = text;
-    this.author = author;
-  }
+  Quote({this.text, this.author}); //constructor same name as class, named parameters
 
-
-  Quote myquote = Quote(author: 'oscar wilde', text: 'this is the quote text');
 }
